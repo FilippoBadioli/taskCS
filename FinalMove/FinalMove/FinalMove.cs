b567c67c553843a2437c5641340c062ca21d5885
FilippoBadioli <filippo.badioli@studio.unibo.it>
2023-06-23 09:50:38 +0200

    added C# files

---
+++ FinalMove/FinalMove/FinalMove.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Timers;
+
+namespace Project
+{
+    public class FinalMove
+    {
+        private static int _delay = 1000;
+        private bool _isActive = false;
+        private System.Timers.Timer _aTimer = new Timer(_delay);
+
+        public FinalMove()
+        {
+            _aTimer.AutoReset = false;
+            _aTimer.Elapsed += OnTimedEvent;
+        }
+
+        public bool IsActive()
+        {
+            return _isActive;
+        }
+
+        public void Trigger()
+        {
+            TowerSingleton.GetInstance().RemoveMoney(10);
+            _isActive = true;
+            _aTimer.Start();
+        }
+
+        private void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            _isActive = false;
+        }
+    }
+
+    class TowerSingleton
+    {
+        private static TowerSingleton instance = null;
+        public int Money { get; private set; } = 200;
+
+        public static TowerSingleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new TowerSingleton();
+            }
+
+            return instance;
+        }
+
+        public void RemoveMoney(int _amount)
+        {
+            Money -= _amount;
+        }
+    }
+
+    public class Test
+    {
+        FinalMove _finalMove = new FinalMove();
+
+
+
+        public static void Main()
+        {
+            FinalMove _finalMove = new FinalMove();
+            var test = new Test();
+            Console.WriteLine(TowerSingleton.GetInstance().Money + ", " + System.DateTime.Now + ", " + _finalMove.IsActive());
+            _finalMove.Trigger();
+            Console.WriteLine(TowerSingleton.GetInstance().Money + ", " + System.DateTime.Now + ", " + _finalMove.IsActive());
+            Console.ReadKey();
+            Console.WriteLine(TowerSingleton.GetInstance().Money + ", " + System.DateTime.Now + ", " + _finalMove.IsActive());
+        }
+    }
+    
+
+} 
---
+++ FinalMove/SpawnManager/SpawnManager.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class WaveManagerSingleton
+    {
+        private long _start;
+        private GameLogicImpl _gameLogicImpl = new GameLogicImpl();
+        private int _waveSize;
+        private Random _random = new Random();
+        private int _type;
+        private int _counter;
+        private float _timerWaves;
+        private static WaveManagerSingleton _instance = null;
+        private int _waveCounter = 2;
+        private float _timeMultiplier = 1.2f;
+
+        private WaveManagerSingleton()
+        {
+            _start = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            _waveSize = _random.Next(2, 4);
+            for (int i = 0; i < _waveSize; i++)
+            {
+                _gameLogicImpl.summonEnemy();
+            }
+            _counter = 0;
+            _timerWaves = 1000;
+        }
+
+        public static WaveManagerSingleton getInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new WaveManagerSingleton();
+            }
+            return _instance;
+        }
+
+        public void spawnWave()
+        {
+            long _now = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            if ((_now - _start) >= _timerWaves)
+            {
+                for (int i = 0; i < _waveSize; i++)
+                {
+                    _gameLogicImpl.summonEnemy();
+                    _start = _now;
+                }
+
+                if (_counter > _waveCounter)
+                {
+                    _waveSize++;
+                    _timerWaves *= _timeMultiplier;
+                    _counter = 0;
+                }
+
+                else
+                {
+                    _counter++;
+
+                }
+            }
+        }
+
+    }
+
+    public class GameLogicImpl
+    {
+        public void summonEnemy()
+        {
+            int _type;
+            Random _random = new Random();
+
+            if (_random.Next(0, 100) < 70)
+            {
+                _type = 0;
+            }
+            else
+            {
+                _type = 1;
+            }
+
+            if (_type == 0)
+            {
+                Console.WriteLine("Goblin Spawned");
+            }
+
+            if (_type == 1)
+            {
+                Console.WriteLine("Wizard Spawned");
+            }
+
+        }
+    }
+
+    class TestSpawn
+    {
+        public static void Main()
+        {
+            WaveManagerSingleton _waveManagerSingleton = WaveManagerSingleton.getInstance();
+            while (true)
+            {
+                char a = Console.ReadKey().KeyChar;
+                if (a == 'e')
+                {
+                    System.Environment.Exit(0);
+                }
+                else
+                {
+                    _waveManagerSingleton.spawnWave();
+                }
+            }
+        }
+    }
+}
